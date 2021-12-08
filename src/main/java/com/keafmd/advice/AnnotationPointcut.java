1a326b7687d2ec94f99d6a5c0b2b72cc60c28d2a
sungoku <2911165699@qq.com>
2021-12-08 13:46:15 +0800

    学习spring aop

--- src/main/java/com/keafmd/advice/AnnotationPointcut.java
+++ src/main/java/com/keafmd/advice/AnnotationPointcut.java
@@ -27,4 +27,17 @@ public class AnnotationPointcut {
         System.out.println("环绕后");
         System.out.println(proceed);
     }
+
+/**
+ * 1.@Before 前置增强（目标方法执行之前，执行注解标注的内容）
+ *
+ * 2.@AfterReturning 后置增强（目标方法正常执行完毕后，执行）
+ *
+ * 3.@Around 环绕增强（目标方法执行前后，分别执行一些代码）
+ *
+ * 4.@AfterThrowing 抛出增强（目标方法发生异常，执行）
+ *
+ * 5.@After Final增强（不管是抛出异常还是正常退出，该增强都会得到执行。一般用于释放资源，相当于try{}finally{}）
+ */
+
 }
--- src/main/java/com/keafmd/controller/StudentController.java
+++ src/main/java/com/keafmd/controller/StudentController.java
@@ -2,6 +2,8 @@ package com.keafmd.controller;
 
 import com.keafmd.entity.StudentInfo;
 import com.keafmd.service.StudentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/stu")
 public class StudentController {
+    //日志
+    private Logger logger =LoggerFactory.getLogger(this.getClass());
     @Autowired
     private StudentService studentService;
 
